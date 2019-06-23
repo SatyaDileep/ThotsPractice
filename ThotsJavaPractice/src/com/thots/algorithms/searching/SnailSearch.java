d6f1ea1f3d804fbb7444902f05fb67391c79fb60
thots <SatyaDileepKumar.Thotakura@in.pega.com>
2019-06-23 20:35:11 +0530

    Adding Searching package and utils package along with basic stuff

---
+++ ThotsJavaPractice/src/com/thots/algorithms/searching/SnailSearch.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.thots.algorithms.searching;
+
+import java.util.Scanner;
+
+import com.thots.utils.FormRandomArrays;
+
+/**
+ * @author thots
+ * Hey there don't get confused with the name.. I'm just the old fashioned linear search
+ */
+public class SnailSearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the number of elements in the array.. \n");
+		Scanner scanner = new Scanner(System.in);
+		int limit = scanner.nextInt();
+		// form random array first
+		int[] inputArray = FormRandomArrays.getArrayData(limit, 100);
+		
+		// just print and see
+		printArray(inputArray);
+		
+		// lets ask for the target..
+		System.out.println("Enter the number to search for.. \n");
+		int target = scanner.nextInt();
+		SnailSearch snailSearch = new SnailSearch();
+		System.out.println(snailSearch.searchForTarget(inputArray, target));
+	}
+	
+	/*
+	 * Pretty print the given array
+	 */
+	private static void printArray(int[] givenArray) {
+		System.out.print("[");
+		for(int i=0; i<givenArray.length; i++) {
+			if(i==givenArray.length-1)
+				System.out.print(givenArray[i]);
+			else
+				System.out.print(givenArray[i]+",");
+		}
+		System.out.print("]");
+	}
+	
+	/**
+	 * @param givenArray
+	 * @param target
+	 * @return if element is found at a position, returns the position else nope
+	 */
+	public String searchForTarget(int[] givenArray, int target) {
+		int index = -1;
+		for(int i=0; i<givenArray.length; i++) {
+			if(givenArray[i] == target) {
+				index = i;
+				break;
+			}
+		}
+		return index == -1 ? "target not found" : "target found at position : "+(index+1);
+	}
+
+}
---
+++ ThotsJavaPractice/src/com/thots/utils/FormRandomArrays.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.thots.utils;
+
+/**
+ * @author thots
+ * Contains a static method to return random arrays of specified size within the range of 0 till multiplying index.
+ * Ex : getArrayData(5,1000); returns an array of 5 numbers raning from 0 till 1000
+ */
+public class FormRandomArrays {
+	/**
+	 * @param size
+	 * @return integer array with random numbers in it
+	 * Note : it doesn't guarantee unique numbers inside it
+	 */
+	public static int[] getArrayData(int size, int multiplyingIndex){
+		int[] arrayOfIntegers = new int[size];
+		int multiplier = multiplyingIndex <= 0? 100 : multiplyingIndex;
+		for (int i = 0; i < arrayOfIntegers.length ; i++)
+		{
+			arrayOfIntegers[i] = (int)(Math.random()*multiplier);
+		}
+		return arrayOfIntegers;
+	}
+	
+	public static void main(String a[]) {
+		// doubt it? just execute me and see
+		getArrayData(5,-1);
+	}
+}
