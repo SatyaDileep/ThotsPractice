f4ba1eb5eb395fb417f6510493adf4b27a3e778e
thots <SatyaDileepKmr30@gmail.com>
2019-06-30 23:21:48 +0530

    pushing in the right solution

--- ThotsJavaPractice/src/com/thots/algorithms/searching/practicestuff/MinInSortedAndRotatedArray.java
+++ ThotsJavaPractice/src/com/thots/algorithms/searching/practicestuff/MinInSortedAndRotatedArray.java
@@ -3,6 +3,8 @@
  */
 package com.thots.algorithms.searching.practicestuff;
 
+import com.thots.utils.ArrayUtils;
+
 /**
  * @author thots
  * A sorted array is rotated at some unknown point, find the minimum element in it.
@@ -19,15 +21,26 @@ public class MinInSortedAndRotatedArray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] inputArray = {5, 6, 0, 1, 2, 3, 4};
+		int[] inputArray = {115, 106, 97,0, -92, 93, 94};
+		ArrayUtils.printArray(inputArray);
+		System.out.println(minElementInArray(inputArray));
 	}
 	
 	private static int minElementInArray(int[] inputArray) {
-		int left=0, right=inputArray.length-1, mid, min=inputArray[0];
+		int left=0, right=inputArray.length-1, mid;
 		while(right >= left) {
 			mid = left+(right-left)/2;
+			System.out.println(left+" is left --"+right+" is right,--calculated mid = "+mid);
+			if(inputArray[mid] < inputArray[mid+1] && inputArray[mid] < inputArray[mid-1]) {
+				return inputArray[mid];
+			}
 			if(inputArray[mid] > inputArray[mid+1]) {
-				
+				// search left
+				left = mid+1;
+			}
+			else if(inputArray[mid] < inputArray[mid+1]) {
+				// search left
+				right = mid-1;
 			}
 		}
 		return -1;
